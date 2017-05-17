0a4a86ce85ac4e681f50b066670ec04be3f38e21
Peter Ajtai <peter.ajtai@thinksolid.com>
2017-05-16 20:13:32 -0700

    cleanup

--- core/index.js
+++ core/index.js
@@ -7,50 +7,43 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-console.log('starting loads');
-let controllers = require('./loadControllers');
-let models = require('./loadModels');
-console.log('models', Object.keys(models));
 
-console.log('start export');
-module.exports = {
-    boot() {
-        for (let controller of Object.keys(controllers)) {
+exports.controllers = require('./loadControllers');
+exports.models = require('./loadModels');
+exports.boot = () => {
+    for (let controller of Object.keys(exports.controllers)) {
 
-            controller = controllers[controller];
-            // Using ifs because ES6 class instance methods are a pain to iterate over - presving optin to use them
-            if (controller.index) {
-                if (controller.default) {
-                    console.log('registering home page - this should only be called once');
-                    app.get('/', controller.index.bind(controller));
-                }
-                app.get(`/${controller.name}/`, controller.index.bind(controller));
+        controller = exports.controllers[controller];
+        // Using ifs because ES6 class instance methods are a pain to iterate over - presving optin to use them
+        if (controller.index) {
+            if (controller.default) {
+                console.log('registering home page - this should only be called once');
+                app.get('/', controller.index.bind(controller));
             }
-            if (controller.create) {
-                app.get(`/${controller.name}/create`, controller.create.bind(controller));
-            }
-            if (controller.store) {
-                app.post(`/${controller.name}`, controller.store.bind(controller));
-            }
-            if (controller.show) {
-                app.get(`/${controller.name}/:${controller.singularName}`, controller.show.bind(controller));
-            }
-            if (controller.edit) {
-                app.get(`/${controller.name}/:${controller.singularName}/edit`, controller.edit.bind(controller));
-            }
-            if (controller.update) {
-                app.put(`/${controller.name}/:${controller.singularName}`, controller.update.bind(controller));
-            }
-            if (controller.destroy) {
-                app.delete(`/${controller.name}/:${controller.singularName}`, controller.destroy.bind(controller));
-            }
-
+            app.get(`/${controller.name}/`, controller.index.bind(controller));
         }
-    },
-    controllers,
-    models
-};
-console.log('exported');
+        if (controller.create) {
+            app.get(`/${controller.name}/create`, controller.create.bind(controller));
+        }
+        if (controller.store) {
+            app.post(`/${controller.name}`, controller.store.bind(controller));
+        }
+        if (controller.show) {
+            app.get(`/${controller.name}/:${controller.singularName}`, controller.show.bind(controller));
+        }
+        if (controller.edit) {
+            app.get(`/${controller.name}/:${controller.singularName}/edit`, controller.edit.bind(controller));
+        }
+        if (controller.update) {
+            app.put(`/${controller.name}/:${controller.singularName}`, controller.update.bind(controller));
+        }
+        if (controller.destroy) {
+            app.delete(`/${controller.name}/:${controller.singularName}`, controller.destroy.bind(controller));
+        }
 
-app.listen(PORT);
-console.log(`Express started on port ${PORT}`);
+    }
+
+
+    app.listen(PORT);
+    console.log(`Express started on port ${PORT}`);
+};
